773480a40c4703623bbb549a4622f2bee5417cf9
Rio Le <rio@shockbyte.com>
2023-07-04 09:10:26 +0000

    Fix: add try catch block to handle got error

--- src/module/base.ts
+++ src/module/base.ts
@@ -1,24 +1,27 @@
-import got from 'got';
-import WhmcsApi from '..';
+import got from "got";
+import WhmcsApi from "..";
 
 export abstract class BaseModule {
-
   async request(methodName: string, options?: any): Promise<any> {
     options.identifier = WhmcsApi.options.identifier;
     options.secret = WhmcsApi.options.secret;
     options.action = methodName;
-    options.responsetype = 'json';
+    options.responsetype = "json";
 
     return new Promise(async (resolve, reject) => {
-      const res = await got(WhmcsApi.options.apiUrl, {
-        method: 'post',
-        form: options
-      });
+      try {
+        const res = await got(WhmcsApi.options.apiUrl, {
+          method: "post",
+          form: options,
+        });
 
-      const data = JSON.parse(res.body);
+        const data = JSON.parse(res.body);
 
-      if (data.result != "success") return reject(data);
-      resolve(data);
+        if (data.result != "success") return reject(data);
+        resolve(data);
+      } catch (error) {
+        reject(error);
+      }
     });
   }
 }
